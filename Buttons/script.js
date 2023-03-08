da44a1e2e43c37ae1c5d9f154098cfa2663e758d
CedricPedric <cedricnelson03@gmail.com>
2023-03-08 11:25:44 +0100

    Assignment is done

---
+++ Buttons/script.js
@@ -0,0 +1,12 @@
+console.log("This shit is linked!")
+
+function greenButton(){
+    document.body.style.backgroundColor="green";
+}
+
+function redButton(){
+    document.body.style.backgroundColor="red";
+}
+function blueButton(){
+    document.body.style.backgroundColor="blue";
+}
